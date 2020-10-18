b0a9017781337df5e28f8e022425fe9fc611a41d
Ming-der Wang <mingderwang@gmail.com>
2020-10-18 06:08:45 +0000

    remove user add post

--- x/myDID/client/rest/rest.go
+++ x/myDID/client/rest/rest.go
@@ -9,4 +9,11 @@ import (
 // RegisterRoutes registers myDID-related REST handlers to a router
 func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router) {
   // this line is used by starport scaffolding # 1
+		r.HandleFunc("/myDID/post", createPostHandler(cliCtx)).Methods("POST")
+		r.HandleFunc("/myDID/post", listPostHandler(cliCtx, "myDID")).Methods("GET")
+		r.HandleFunc("/myDID/post/{key}", getPostHandler(cliCtx, "myDID")).Methods("GET")
+		r.HandleFunc("/myDID/post", setPostHandler(cliCtx)).Methods("PUT")
+		r.HandleFunc("/myDID/post", deletePostHandler(cliCtx)).Methods("DELETE")
+
+		
 }
---
+++ x/myDID/handlerMsgCreatePost.go
@@ -0,0 +1,20 @@
+package myDID
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/mingderwang/myDID/x/myDID/types"
+	"github.com/mingderwang/myDID/x/myDID/keeper"
+)
+
+func handleMsgCreatePost(ctx sdk.Context, k keeper.Keeper, msg types.MsgCreatePost) (*sdk.Result, error) {
+	var post = types.Post{
+		Creator: msg.Creator,
+		ID:      msg.ID,
+    	Title: msg.Title,
+    	Body: msg.Body,
+	}
+	k.CreatePost(ctx, post)
+
+	return &sdk.Result{Events: ctx.EventManager().Events()}, nil
+}
